582ad7c2e483baa2de4c7df805efe8c73c0e192c
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-16 17:21:01 +0530

    Making basic structure

--- src/App.js
+++ src/App.js
@@ -3,38 +3,20 @@ import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
-
-const Cursor = styled(motion.div)`
-  height: 2rem;
-  width: 2rem;
-  border-radius: 50%;
-  background-color: black;
-`
-
 function App() {
 
-  const animation = {
-    hidden:{
-      y: 0, 
-      scale: 1
-    },
-    visible:{
-      y: [0, 0, 0, 0, 50, 50, 50, 50, 100, 100, 100, 100, 0],
-      scale:[1, 1, 1, 1, 1, 0.6, 1, 1, 1, 1, 1, 1, 1, 0.6, 1, 1, 1, 1, 1, 1, 1, 0.6, 1, 1, 1, 1, 1, 1],
-
-      transition:{
-        duration: 12
-      }
-    }
-  }
   return (
-    <div className="App">
-      <Cursor
-        variants={animation}
-        initial="hidden"
-        animate="visible"
-      >
-      </Cursor>
+    <div className="container flex w-full h-screen">
+      <div className="left_container flex flex-col justify-start items-end w-1/2 h-full bg-blue-300 p-16">
+        <span className="text-gray-400 text-md font-normal w-72 gap-6">Choose only the options that have been applied incorrectly</span>
+        <div className="inner_container flex justify-start items-center gap-4 border-2 border-red-300">
+          <div className="h-6 w-6 rounded border-2 border-gray-500"></div>
+          <span className="text-white text-md font-normal">White Space</span>
+        </div>
+      </div>
+      <div className="right_container flex flex-col justify-start w-1/2 h-full bg-gray-300 p-10">
+
+      </div>
     </div>
   );
 }
